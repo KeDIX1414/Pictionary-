6a868a04be6703f1592b7cfa760b8d2ee3a0c4f1
KeDIX1414 <kathdix@seas.upenn.edu>
2016-07-13 00:09:30 -0400

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,145 @@
+var socket = io();
+var canvas = document.getElementById('canvas')
+var button = document.getElementById('button')
+var paint = false
+var currentX = 0
+var currentY = 0
+
+// Find the color the user has selected
+function getColor() {
+	var select = document.getElementById("selector_one");
+	var option = select.options[select.selectedIndex].value;
+	var color = ""
+	switch (option) {
+		case "red":
+			color = "red"
+			break
+		case "blue":
+			color = "blue"
+			break
+		case "black":
+			color = "black"
+			break
+		case "brown":
+			color = "brown"
+			break
+		case "orange":
+			color = "orange"
+			break
+		case "yellow":
+			color = "yellow"
+			break
+		case "purple":
+			color = "purple"
+			break
+		case "green":
+			color = "green"
+			break
+	}
+	return color
+}
+
+/*
+The following functions and event listeners are used to find the coordinates where a player is drawing, and transmit those 
+coordinates to the server. The server is responsible for sending the coordinates to the necessary players so the image can
+be drawn on their screens in real time. If a player is not designated as a "drawer", the server will not send the coordinates.*/
+
+canvas.addEventListener('click', function(e) {
+
+	var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = canvas;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    x = e.pageX - totalOffsetX;
+    y = e.pageY - totalOffsetY;
+	var coordinates = {xPos: x, yPos: y};
+	socket.emit('sentclick', coordinates);
+
+}, false);
+
+socket.on('drawclick', function(coordinates) {
+	context = canvas.getContext("2d")
+	context.fillStyle = coordinates.colors
+	context.fillRect(coordinates.xPos, coordinates.yPos, 2, 2)
+});
+
+canvas.addEventListener('mousedown', function(e) {
+	paint = true;
+	var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = canvas;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    x = e.pageX - totalOffsetX;
+    y = e.pageY - totalOffsetY;
+	currentX = x
+	currentY = y
+	var coordinates = {xPos: x, yPos: y, colors: getColor()};
+	socket.emit('sentclick', coordinates);
+
+}, false);
+
+canvas.addEventListener('mousemove', function(e) {
+	if (paint) {
+		var totalOffsetX = 0;
+    	var totalOffsetY = 0;
+    	var canvasX = 0;
+    	var canvasY = 0;
+    	var currentElement = canvas;
+
+    	do{
+        	totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        	totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    	}
+    	while(currentElement = currentElement.offsetParent)
+
+    	x = e.pageX - totalOffsetX;
+    	y = e.pageY - totalOffsetY;
+		var coordinates = {xInit: currentX, yInit: currentY, xFin: x, yFin: y, colors: getColor()};
+		socket.emit('sentline', coordinates);
+		currentX = x
+		currentY = y
+
+	}
+}, false);
+
+socket.on('drawline', function(coordinates) {
+	context = canvas.getContext("2d")
+	context.strokeStyle = coordinates.colors
+	context.beginPath()
+	context.moveTo(coordinates.xInit, coordinates.yInit)
+	context.lineTo(coordinates.xFin, coordinates.yFin)
+	context.lineWidth = 2
+	context.stroke()
+});
+
+canvas.addEventListener('mouseout', function(e) {
+	paint = false;
+}, false);
+
+canvas.addEventListener('mouseup', function(e) {
+	paint = false;
+}, false);
+
+socket.on('clearcanvas', function() {
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+
+
--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http); //app
+var sockets
 var players = {}; // keeps track of everyone playing the game
-var id = 1; // is updated to give every player a unique id
+var sockets = new Array();
+var numPlayers = 0;
+var playing = false;
+var round = 1;
 
 app.get('/', function(req, res){
 	res.sendfile('PictionarySite.html');
@@ -12,31 +16,49 @@ app.get('/', function(req, res){
 
 app.use(express.static('public'));
 
+// Give players time to log in. When timer stops, the game begins.
+setTimeout(startGame, 20000);
+
 // controls what to do when players connect and disconnect
 io.on('connection', function(socket){
+  numPlayers = numPlayers + 1;
 	console.log('a user connected');
-
-	// create a new player
-	var player = new Player(id, "waiting", "drawer");
- 	players[socket] = player;
- 	id = id + 1;
- 	console.log(players[socket].id + "  " + players[socket].status + "  " + players[socket].type);
-  	
+  console.log("" + socket.id)
+  sockets.push(socket.id);
+	
  	// handle when player disconnects
   	socket.on('disconnect', function(){
-  		delete players[socket];
+  		delete players.get(socket);
     	console.log('user disconnected');
     });
 
     // handle when someone clicks
     socket.on("sentclick", function (coordinates) {
-    	socket.broadcast.emit('drawclick', coordinates);
+      if (!playing) { 
+        return; 
+      }
+      var player = players[socket.id];
+      //var player = players.get(socket)
+      //console.log(socket.id + "   " + players[socket].myID + "  " + players[socket].status + "  " + players[socket].type);
+      if (player.type === 'drawer') {
+    	  //io.sockets.emit('drawclick', coordinates);
+        socket.emit('drawclick', coordinates);
+        socket.broadcast.to(players[socket.id].partner).emit('drawclick', coordinates);
+      }
     });
 
     // handle when a line is drawn
     socket.on("sentline", function (coordinates) {
-      console.log("line is about to be drawn")
-      socket.broadcast.emit('drawline', coordinates);
+      if (!playing) {
+        return;
+      }
+      var player = players[socket.id];
+      //var player = players.get(socket)
+      if (player.type === 'drawer') {
+        //io.sockets.emit('drawline', coordinates);
+        socket.emit('drawline', coordinates);
+        socket.broadcast.to(players[socket.id].partner).emit('drawline', coordinates);
+      }
     });
 
 });
@@ -52,9 +74,54 @@ process.on( 'SIGINT', function() {
     process.exit( );
 });
 
+/* This section handles the Pictionary gameplay logic.*/
 
-function Player(id, status, type) {
-	this.id = id;
-	this.status = status;
-	this.type = type;
+function startGame() {
+  // Set up the initial game play. Pair people together.
+  console.log("Game is starting!");
+  if (numPlayers % 2 != 0) {
+    console.log("Shutting down. Can't play with an odd number of players!!!");
+    process.exit();
+  }
+  for (i = 0; i < sockets.length; i++) {
+    //console.log(players[sockets[i - 1]]);
+    if (i % 2 === 0) {
+      console.log("drawer" + "  " + sockets[i].id);
+      players[sockets[i]] = {type: "drawer", partner: sockets[i + 1]};
+    } else {
+      console.log("guesser" + "  " + sockets[i].id);
+      players[sockets[i]] = {type: "guesser", partner: sockets[i - 1]} ;
+
+    }
+  }
+  for (i = 0; i < sockets.length; i++) {
+    console.log(players[sockets[i]])
+  }
+  playing = true;
+  var time = setInterval(swapPartners, 10000)
+
+  function swapPartners() {
+    playing = false;
+    io.sockets.emit('clearcanvas');
+    console.log("Swapping players ");
+    for (i = 0; i < sockets.length; i++) {
+      if (players[sockets[i]].type === "guesser") {
+        players[sockets[i]].type = "drawer";
+      } else {
+        players[sockets[i]].type = "guesser";
+      }
+    }
+    round = round + 1;
+    if (round === 5) {
+      clearInterval(time);
+      endGame();
+    }
+    playing = true;
+  }
+}
+
+
+function endGame() {
+  playing = false;
+  process.exit();
 }
